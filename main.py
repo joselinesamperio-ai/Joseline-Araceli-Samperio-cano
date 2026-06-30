cb746094e9de69dda17a107c41d53d0c9832a589
joselinesamperio-ai <joselinesamperio@gmail.com>
2026-06-29 22:04:16 -0600

    Add greeting function for project collaborators

---
+++ main.py
@@ -0,0 +1,8 @@
+def saludar_colaboradores():
+    print("=========================================")
+    print("Proyecto Joseline Araceli Samperio Cano.")
+    print("Este es un entorno controlado de versiones.")
+    print("=========================================")
+
+if __name__ == "__main__":
+    saludar_colaboradores()
